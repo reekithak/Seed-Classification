0c3612dfcb05b3bf62eb84e2194307ca03e6ea77
Akhil Sanker <reekithak@gmail.com>
2021-04-04 04:17:32 +0530

    new

--- upload.py
+++ upload.py
@@ -38,5 +38,5 @@ def upload():
     return render_template('upload.html')
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True,port=9696)
+if __name__ == "__main__":
+    app.run(debug=True)
